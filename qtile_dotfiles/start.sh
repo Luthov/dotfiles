ad75bfb2e6b3aaad9e6cf84d6889a4a3c1dfc8ef
Luthov <lukettl15@gmail.com>
2023-04-25 14:23:06 +0800

    Added more files

--- qtile_dotfiles/start.sh
+++ qtile_dotfiles/start.sh
@@ -6,7 +6,9 @@ mkdir ~/Github/
 cd Github/
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-yay -S alacritty lightdm qtile zsh pavucontrol picom flameshot neovim playerctl catppuccin-gtk-theme-macchiato papirus-folders-catppuccin-git network-manager-applet
-
+yay -S alacritty lightdm qtile zsh pavucontrol picom flameshot neovim playerctl catppuccin-gtk-theme-macchiato papirus-folders-catppuccin-git network-manager-applet gvfs polybar
 
 chsh -s $(which zsh)
+
+cd ..
+git clone https://github.com/Luthov/dotfiles
