9876e444b78865f5b8116c467aa0c7c748ee97e7
Luthov <lukettl15@gmail.com>
2023-04-26 20:12:29 +0800

    Moved files and updated polybar config

--- qtile_dotfiles/nvim/lua/core/keymaps.lua
+++
@@ -1,8 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.number = true
-vim.opt.showcmd = true
-vim.opt.cursorline = true
-vim.api.nvim_set_keymap('n', '<leader>t', ':ToggleTerm<CR>', {noremap = true})
--- qtile_dotfiles/qtile/autostart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-xrandr --output DP-2 --mode 1920x1080 --rate 144
-nm-applet &
-polybar &
-picom &
